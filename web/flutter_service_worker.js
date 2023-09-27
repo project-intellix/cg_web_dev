2fc8bd0fb40bd2be420fd8caa15c3e608e8824b3
Sportkanone123 <kim-mueller@gmx.com>
2023-09-27 19:43:54 +0200

    performance improvements 2

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -83,8 +83,7 @@ const RESOURCES = {"manifest.json": "0cc81f760daec001cc325ab45812ffa4",
 "favicon.png": "c2eb0d922b17be757a21c85dcbcbcbd4"};
 // The application shell files that are downloaded before a service worker can
 // start.
-const CORE = ["/",
-"main.dart.js",
+const CORE = ["main.dart.js",
 "index.html",
 "assets/AssetManifest.json",
 "assets/FontManifest.json"];
